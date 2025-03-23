99e47ceccff532ce599a3165e01f9cf8281de243
Evangelos Kassos <github@evangeloskassos.com>
2025-03-23 16:06:57 -0400

    chore/ Add `latexminted` dependency & shell escape optimizations (#30)
    Add `latexminted` package dependency to ensure that `minted` v3 can run
    correctly on all systems. Only use shell escape when using an
    unsupported system (TeX Live < 2024 or MiKTeX).

--- swift_book_pdf/pdf.py
+++ swift_book_pdf/pdf.py
@@ -12,12 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import logging
 import os
+import re
 import subprocess
 
 from swift_book_pdf.config import Config
 from swift_book_pdf.fonts import check_for_missing_font_logs
 from swift_book_pdf.log import run_process_with_logs
+
+logger = logging.getLogger(__name__)
 
 
 class PDFConverter:
@@ -26,6 +30,44 @@ class PDFConverter:
             os.path.dirname(os.path.abspath(__file__)), "assets"
         )
         self.config = config
+
+    def get_latex_command(self) -> list[str]:
+        command = ["lualatex", "--interaction=nonstopmode"]
+
+        pattern = r"(TeX Live|MiKTeX) (\d{2,4})"
+
+        result = subprocess.run(
+            ["lualatex", "--version"],
+            capture_output=True,
+            text=True,
+        )
+
+        if result.returncode != 0:
+            raise RuntimeError(f"Failed to get LaTeX version: {result.stderr}")
+
+        match = re.search(pattern, result.stdout)
+        if match:
+            tex_distribution = match.group(1)
+            tex_version = match.group(2)
+
+            if tex_distribution == "TeX Live":
+                if int(tex_version) < 2024:
+                    command.append("--shell-escape")
+                    command.append("--enable-write18")
+            elif tex_distribution == "MiKTeX":
+                command.append("--shell-escape")
+                command.append("--enable-write18")
+            else:
+                raise RuntimeError(
+                    f"Unsupported LaTeX distribution: {tex_distribution}"
+                )
+        else:
+            raise RuntimeError(f"Failed to get LaTeX version: {result.stderr}")
+        logger.debug(
+            f"Using LaTeX distribution: {tex_distribution}, version: {tex_version}"
+        )
+        logger.debug(f"LaTeX Command: {command}")
+        return command
 
     def convert_to_pdf(self, latex_file_path: str) -> None:
         env = os.environ.copy()
@@ -39,7 +81,7 @@ class PDFConverter:
         )
 
         process = subprocess.Popen(
-            ["lualatex", "--shell-escape", "--enable-write18", latex_file_path],
+            self.get_latex_command() + [latex_file_path],
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
             text=True,
